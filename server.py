474a4a2cc906df51a6391aee311f7967fb72741e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-03 18:06:00 -0700

    more templates

--- server.py
+++ server.py
@@ -26,6 +26,45 @@ def index():
     return render_template("homepage.html")
 
 
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route("/register")
+def show_registration_form():
+    """Shows registration form"""
+
+    return render_template("registration_form.html")
+
+
+@app.route("/register", methods=["POST"])
+def adds_user_to_db():
+    """Adds user to db"""
+    email = request.form("email")
+    password = request.form("password")
+    age = request.form("age")
+    zipcode = request.form("zipcode")
+
+    is_user_there = db.session.query(User).filter(User.email=email).all()
+
+    # if email in is_user_there:
+    #     fails
+    # else:
+    #     add to db/make instance of class
+
+# check if email in table already
+    # add user to table by instantiating an instance of user class
+    # commit to db
+    # fash message - success, you may now rate movies
+# else return to registration if email is already in db
+
+    return redirect("/")
+
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
